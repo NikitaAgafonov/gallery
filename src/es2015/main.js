88e9a8c4edf588bc4f4170f5a738e8ec17466771
NikitaAgafonov <overcome2014@mail.ru>
2016-12-27 11:53:55 +0700

    fix size img, btn-group, code-style, promise, build

--- src/es2015/main.js
+++ src/es2015/main.js
@@ -1,58 +1,84 @@
 const module = angular.module('gallery', ['ngRoute','cfp.hotkeys']),
-		url = "https://api-fotki.yandex.ru/api/top/?format=json";
-
+		url = 'https://api-fotki.yandex.ru/api/top/?format=json',
+		countImg = 49,
+		countImgPage = 12;
 
 let nowPage = 0,
 	watchImgId = 0,
 	pages = [];
 
 module.config(function($routeProvider) {
+
 	$routeProvider
+
 		.when('/', {
 			templateUrl: 'photos.html',
 			controller: 'photos'
 		})
+
 		.when('/photo:id', {
 			templateUrl: 'photo.html',
 			controller: 'photo'
 		})
+
 		.when('/page:num', {
 			templateUrl: 'photos.html',
 			controller: 'photos'
 		})
+
 		.otherwise({
 			template: '<center><h1>404 NOT FOUND</h1></center>'
 		})
+
 });
 
 module.config(function($sceDelegateProvider) {
+
 	$sceDelegateProvider.resourceUrlWhitelist([
 		'self',
 		url
 	]);
+
 });
 
 
 module.controller('photos', function($scope,$http,$routeParams,$location,$sce,getData){
+
 	$scope.nowPage = nowPage;
 	$scope.watchImgId = watchImgId;
+
+
 	if (!pages[0]) {
+
 		$http({
 			method: 'JSONP',
 			url: url
-		}).then(function (response) {
-			$scope.data = response.data.entries;
-			getData.getPages($scope);
-			pages = $scope.pages;
-			if($routeParams.num) {
-				nowPage = $routeParams.num-1;
-				$scope.nowPage = $routeParams.num-1;
+		})
+
+		.then(function (response) {
+
+				$scope.data = response.data.entries;
+				getData.getPages($scope);
+				pages = $scope.pages;
+
+				if($routeParams.num) {
+					nowPage = $routeParams.num-1;
+					$scope.nowPage = $routeParams.num-1;
+				}
 			}
-		}, function (err) {
-			console.log(err);
-		});
+		)
+
+		.catch((err) => {
+
+				throw err.status+': '+err.statusText;
+
+			}
+		);
+
 	} else {
+
 		$scope.pages = pages;
+
 		if($routeParams.num) {
 			nowPage = $routeParams.num-1;
 			$scope.nowPage = $routeParams.num-1;
@@ -61,90 +87,132 @@ module.controller('photos', function($scope,$http,$routeParams,$location,$sce,ge
 });
 
 module.controller('photo', function($scope,$http,$routeParams,$location,$sce,getData,hotkeys){
+
+
 	$scope.nowPage = nowPage;
 	$scope.watchImgId = watchImgId;
+	let photoNum;
+
+
 	$scope.selectPhoto = function (num) {
+
 		if (num) {
-			if (watchImgId<=48) {
+
+			if (watchImgId<countImg) {
 				watchImgId++;
 				$location.path('/photo'+watchImgId);
 			}
+
 		} else {
+
 			if (watchImgId>0) {
 				watchImgId--;
 				$location.path('/photo'+watchImgId);
 			}
+
 		}
 	};
+
+
 	hotkeys.add({
+
 		combo: 'left',
 		description: 'prevPhoto',
+
 		callback: function() {
 			if (watchImgId>0) {
 				watchImgId--;
 				$location.path('/photo'+watchImgId);
 			}
 		}
+
 	});
+
+
 	hotkeys.add({
+
 		combo: 'right',
 		description: 'nextPhoto',
+
 		callback: function() {
-			if (watchImgId<=48) {
+			if (watchImgId<countImg) {
 				watchImgId++;
 				$location.path('/photo'+watchImgId);
 			}
 		}
+
 	});
+
+
 	if (!pages[0]) {
+
 		$http({
 			method: 'JSONP',
 			url: url
-		}).then(function (response) {
-			$scope.data = response.data.entries;
-			getData.getPages($scope);
-			pages = $scope.pages;
-			if($routeParams.id) {
-				watchImgId = $routeParams.id;
-				nowPage = Math.floor(watchImgId/12);
-				$scope.watchImgId = watchImgId;
-				$scope.nowPage = nowPage;
-				let photoNum = watchImgId-nowPage*12;
-				$scope.photo = $scope.pages[nowPage][photoNum];
+		})
+
+		.then(function (response) {
+
+				$scope.data = response.data.entries;
+				getData.getPages($scope);
+				pages = $scope.pages;
+
+				if($routeParams.id) {
+					watchImgId = $routeParams.id;
+					nowPage = Math.floor(watchImgId/countImgPage);
+					$scope.watchImgId = watchImgId;
+					$scope.nowPage = nowPage;
+					photoNum = watchImgId-nowPage*countImgPage;
+					$scope.photo = $scope.pages[nowPage][photoNum];
+				}
 			}
-		}, function (err) {
-			console.log(err);
-		});
+		)
+
+		.catch((err) => {
+				throw err.status+': '+err.statusText;
+			}
+		);
+
 	} else {
+
 		$scope.pages = pages;
+
 		if($routeParams.id) {
 			watchImgId = $routeParams.id;
-			nowPage = Math.floor(watchImgId/12);
+			nowPage = Math.floor(watchImgId/countImgPage);
 			$scope.watchImgId = watchImgId;
 			$scope.nowPage = nowPage;
-			let photoNum = watchImgId-nowPage*12;
+			photoNum = watchImgId-nowPage*countImgPage;
 			$scope.photo = $scope.pages[nowPage][photoNum];
 		}
 	}
-
 });
 
 module.factory('getData', function($http){
+
 	return {
 		getPages: function ($scope) {
+
 			$scope.pages = [];
-			let j = 0;
-			for(let i=0; i <= (Math.ceil($scope.data.length/12))-1; i++) {
+			let j = 0,
+				i = 0;
+
+			for(; i <= (Math.ceil($scope.data.length/countImgPage))-1; i++) {
+
 				$scope.pages[i] = [];
-				for (let z=0;z<12;z++) {
+
+				for (let z=0;z<countImgPage;z++) {
+
 					if ($scope.data[j]) {
 						$scope.pages[i].push($scope.data[j]);
 					} else {
 						break;
 					}
+
 					j++;
 				}
 			}
 		}
 	};
+
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var _module = angular.module('gallery', ['ngRoute', 'cfp.hotkeys']),
-    url = "https://api-fotki.yandex.ru/api/top/?format=json";
+    url = 'https://api-fotki.yandex.ru/api/top/?format=json',
+    countImg = 49,
+    countImgPage = 12;
 
 var nowPage = 0,
     watchImgId = 0,
     pages = [];
 
 _module.config(function ($routeProvider) {
+
 	$routeProvider.when('/', {
 		templateUrl: 'photos.html',
 		controller: 'photos'
@@ -23,29 +26,38 @@ _module.config(function ($routeProvider) {
 });
 
 _module.config(function ($sceDelegateProvider) {
+
 	$sceDelegateProvider.resourceUrlWhitelist(['self', url]);
 });
 
 _module.controller('photos', function ($scope, $http, $routeParams, $location, $sce, getData) {
+
 	$scope.nowPage = nowPage;
 	$scope.watchImgId = watchImgId;
+
 	if (!pages[0]) {
+
 		$http({
 			method: 'JSONP',
 			url: url
 		}).then(function (response) {
+
 			$scope.data = response.data.entries;
 			getData.getPages($scope);
 			pages = $scope.pages;
+
 			if ($routeParams.num) {
 				nowPage = $routeParams.num - 1;
 				$scope.nowPage = $routeParams.num - 1;
 			}
-		}, function (err) {
-			console.log(err);
+		}).catch(function (err) {
+
+			throw err.status + ': ' + err.statusText;
 		});
 	} else {
+
 		$scope.pages = pages;
+
 		if ($routeParams.num) {
 			nowPage = $routeParams.num - 1;
 			$scope.nowPage = $routeParams.num - 1;
@@ -54,86 +66,114 @@ _module.controller('photos', function ($scope, $http, $routeParams, $location, $
 });
 
 _module.controller('photo', function ($scope, $http, $routeParams, $location, $sce, getData, hotkeys) {
+
 	$scope.nowPage = nowPage;
 	$scope.watchImgId = watchImgId;
+	var photoNum = void 0;
+
 	$scope.selectPhoto = function (num) {
+
 		if (num) {
-			if (watchImgId <= 48) {
+
+			if (watchImgId < countImg) {
 				watchImgId++;
 				$location.path('/photo' + watchImgId);
 			}
 		} else {
+
 			if (watchImgId > 0) {
 				watchImgId--;
 				$location.path('/photo' + watchImgId);
 			}
 		}
 	};
+
 	hotkeys.add({
+
 		combo: 'left',
 		description: 'prevPhoto',
+
 		callback: function callback() {
 			if (watchImgId > 0) {
 				watchImgId--;
 				$location.path('/photo' + watchImgId);
 			}
 		}
+
 	});
+
 	hotkeys.add({
+
 		combo: 'right',
 		description: 'nextPhoto',
+
 		callback: function callback() {
-			if (watchImgId <= 48) {
+			if (watchImgId < countImg) {
 				watchImgId++;
 				$location.path('/photo' + watchImgId);
 			}
 		}
+
 	});
+
 	if (!pages[0]) {
+
 		$http({
 			method: 'JSONP',
 			url: url
 		}).then(function (response) {
+
 			$scope.data = response.data.entries;
 			getData.getPages($scope);
 			pages = $scope.pages;
+
 			if ($routeParams.id) {
 				watchImgId = $routeParams.id;
-				nowPage = Math.floor(watchImgId / 12);
+				nowPage = Math.floor(watchImgId / countImgPage);
 				$scope.watchImgId = watchImgId;
 				$scope.nowPage = nowPage;
-				var photoNum = watchImgId - nowPage * 12;
+				photoNum = watchImgId - nowPage * countImgPage;
 				$scope.photo = $scope.pages[nowPage][photoNum];
 			}
-		}, function (err) {
-			console.log(err);
+		}).catch(function (err) {
+			throw err.status + ': ' + err.statusText;
 		});
 	} else {
+
 		$scope.pages = pages;
+
 		if ($routeParams.id) {
 			watchImgId = $routeParams.id;
-			nowPage = Math.floor(watchImgId / 12);
+			nowPage = Math.floor(watchImgId / countImgPage);
 			$scope.watchImgId = watchImgId;
 			$scope.nowPage = nowPage;
-			var photoNum = watchImgId - nowPage * 12;
+			photoNum = watchImgId - nowPage * countImgPage;
 			$scope.photo = $scope.pages[nowPage][photoNum];
 		}
 	}
 });
 
 _module.factory('getData', function ($http) {
+
 	return {
 		getPages: function getPages($scope) {
+
 			$scope.pages = [];
-			var j = 0;
-			for (var i = 0; i <= Math.ceil($scope.data.length / 12) - 1; i++) {
+			var j = 0,
+			    i = 0;
+
+			for (; i <= Math.ceil($scope.data.length / countImgPage) - 1; i++) {
+
 				$scope.pages[i] = [];
-				for (var z = 0; z < 12; z++) {
+
+				for (var z = 0; z < countImgPage; z++) {
+
 					if ($scope.data[j]) {
 						$scope.pages[i].push($scope.data[j]);
 					} else {
 						break;
 					}
+
 					j++;
 				}
 			}
